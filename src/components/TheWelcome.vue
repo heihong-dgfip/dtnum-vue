97ff9e5d81683e0df8add6543e2555274ea924bc
heihong-dgfip <hei-hong.traing@dgfip.finances.gouv.fr>
2022-07-27 10:52:41 +0200

    fix import

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -5,6 +5,6 @@ import { FrAccordion } from '@dtnum/vue';
 </script>
 
 <template>
-  <fr-accordion
-      ><span slot="title" id="titre1">titre1</span> <span id="descripton1"><span>descripton</span></span></fr-accordion>
+  <FrAccordion
+      ><span slot="title" id="titre1">titre1</span> <span id="descripton1"><span>descripton</span></span></FrAccordion>
 </template>
