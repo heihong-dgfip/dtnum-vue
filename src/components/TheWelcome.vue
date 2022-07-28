4ff17db082977ba637e7e2a68d60fa51458668ca
heihong-dgfip <hei-hong.traing@dgfip.finances.gouv.fr>
2022-07-28 15:53:25 +0200

    accordion change

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
 import { FrAccordion } from '@dtnum/vue';
+import { ref } from 'vue'
+
+const descripton = ref('descripton')
+
+function setdata(){
+    descripton.value = 'descripton2'
+}
 
 
 </script>
 
 <template>
   <FrAccordion
-      ><span slot="title" id="titre1">titre1</span> <span id="descripton1"><span>descripton</span></span></FrAccordion>
+      ><span slot="title" id="titre1">titre1</span> <span id="descripton1"><span>{{descripton}}</span></span></FrAccordion>
+      <span>{{descripton}}</span>
+      <button @Click="setdata">click</button>
 </template>
+
