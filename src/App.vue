452e6a251cd1b53523cb78e4229b0a67f6d071e3
heihong-dgfip <hei-hong.traing@dgfip.finances.gouv.fr>
2022-08-05 18:04:04 +0200

    routing

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { FrHeader, FrHeaderService, FrNavigation } from '@dtnum/vue';
 </script>
 
 <template>
-  <header>
+   <FrHeader>
+      <FrHeaderService
+        slot="service"
+        href-link="/"
+        aria-current-link="page"
+        title-link="Accueil - Système de Design de l'État"
+        text-link="Système de Design de l'État"
+      >
+      </FrHeaderService>
+      <FrNavigation>
+        <ul class="fr-nav__list">
+          <li class="fr-nav__item">
+              <RouterLink to="/" class="fr-nav__link">Home</RouterLink>
+          </li>
+          <li class="fr-nav__item">
+           <RouterLink to="/about" class="fr-nav__link">About</RouterLink>
+          </li>
+        </ul>
+      </FrNavigation>
+    </FrHeader>
+  <!-- <header>
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -15,70 +34,6 @@ import HelloWorld from './components/HelloWorld.vue'
       </nav>
     </div>
   </header>
-
+ -->
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
