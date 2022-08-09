0c2ec5f33067e91a46ad021d0682a26da3638947
heihong-dgfip <hei-hong.traing@dgfip.finances.gouv.fr>
2022-08-09 13:44:08 +0200

    test header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { FrHeader, FrHeaderService, FrNavigation } from '@dtnum/vue';
 </script>
 
 <template>
-  <header>
+  <!-- <header>
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -13,72 +13,26 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+      
     </div>
-  </header>
-
-  <RouterView />
+  </header> 
+    <RouterView />-->
+  
+  <FrHeader>
+     <FrHeaderService
+        slot="service"
+        href-link="/"
+        aria-current-link="page"
+        title-link="Accueil - Système de Design de l'État"
+        text-link="Système de Design de l'État"
+      >
+      </FrHeaderService>
+      <FrNavigation>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </FrNavigation>
+    
+  </FrHeader>
+<RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import { defineCustomElements } from '@dtnum/core/loader';
 
-import './assets/main.css'
 
 const app = createApp(App)
 
